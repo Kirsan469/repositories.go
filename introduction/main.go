5eb9d05a019216b2173df2482cc8ce4db726edfa
Kirill Sorokin <kirsan469@gmail.com>
2022-07-25 23:19:03 +0300

    init commit

--- introduction/main.go
+++ introduction/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,11 +11,17 @@ func main() {
 	MakeRequest()
 }
 
-func MakeRequest() {
-	resp, err := http.Get("pi.openweathermap.org/data/3.0/onecall/timemachine/get")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	url := "https://api-football-v1.p.rapidapi.com/v3/timezone"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-RapidAPI-Key", "07ee356bf3mshb3f33ef73f6db25p15586djsn9878a111fec8")
+	req.Header.Add("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
